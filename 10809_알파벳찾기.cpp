e237e6ce0bcac08f711abe77ea9d807043fa02ec
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-06-25 22:33:52 +0900

    Add files via upload

---
+++ 10809_알파벳찾기.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+char alpabet[26] = {'a','b','c','d','e','f','g',
+                    'h','i','j','k','l','m','n',
+                    'o','p','q','r','s','t','u','v',
+                    'w','x','y','z'};
+
+int main(){
+    string buffer;
+    int check = 0;
+    cin >> buffer;
+    for(int i = 0; i < 26; i++){
+        for(int j = 0; j < buffer.length(); j++){
+            if(alpabet[i] == buffer[j]){
+                cout << j << " ";
+                check = 0;
+                break;
+            }else{
+                check = 1;
+            }
+            
+        }
+        if(check == 1){
+            cout << -1 << " ";
+        }
+    }
+    return 0;
+}
