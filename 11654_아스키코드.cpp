18b14230b6ac55198c16b38a0d86040278ff7196
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-06-26 10:53:44 +0900

    Update 11654_아스키코드.cpp

--- 11654_아스키코드.cpp
+++ 11654_아스키코드.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
+/*
+char 변수형은 아스키 코드값을 정수 형태로 저장
+char : 'a'
+(int)char : a의 정수형태인 65가 나오는 거임
+*/
 int main(){
     char target;
     cin >> target;
-    cout << (int)target << endl;
+    cout << (int)target << endl; 
 }
- //char 변수는 1바이트 정수다. 그러나 char 자료형이 정수일지라도 일반 정소와는 다른 방식으로
- //사용한다. 그래서 char 값을 정수로 해석하는 대신 아스키 코드 문자로 해석한다
+ 
+ 
