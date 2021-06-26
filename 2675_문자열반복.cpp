82f008f158d131bf2848d1d52737f5a05a2e1b4c
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-06-26 11:26:49 +0900

    Update 2675_문자열반복.cpp

--- 2675_문자열반복.cpp
+++ 2675_문자열반복.cpp
@@ -5,11 +5,11 @@ int main(){
     int NumTestCases; //테스트케이스 개수
     cin >> NumTestCases;
     for(int i = 0; i < NumTestCases; i++){
-        int cnt; //출력반복횟수     
-        string buffer; //출력문자열
-        cin >> cnt >> buffer; 
-        for(int i = 0; i < buffer.length(); i++){
-            for(int j = 0; j < cnt; j++){
+        int cnt; // 반복할 횟수    
+        string buffer; // 입력받을 문자열
+        cin >> cnt >> buffer;
+        for(int i = 0; i < buffer.length(); i++){ //문자열의 길이만큼
+            for(int j = 0; j < cnt; j++){ //각 문자를 cnt만큼 출력한다
                 cout << buffer[i];    
             }
         }
