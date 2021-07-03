8cb2abfc9e9acfd850fe8a1822ffed09df478667
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-07-03 23:26:07 +0900

    Update 2292_벌집.cpp

--- 기본 수학1/2292_벌집.cpp
+++ 기본 수학1/2292_벌집.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-
-
-    
+    int num;
+    cin >> num;
+    if(num == 1){
+        cout << 1 << endl;
+        return 0;
+    }
+    int cnt = 0;
+    int block = 0;
+    while(cnt < num){
+        block++;
+        cnt = 1 + ((6+6*block)*block)/2;
+    }
+    cout << block+1 << endl;
+    return 0;
 }
+
+//velog 코딩리뷰 하면서 반복문 다시 한번 확인하기
+// 등차수열, 바퀴수만 고려하면됨
